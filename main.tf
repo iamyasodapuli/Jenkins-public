12be0dbfbcff3be13607c2aaa62fe8c57c65dc79
iamyasodapuli <yasodapulicsk7@gmail.com>
2025-03-25 11:47:39 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
-resource "google_storage_bucket" "my-bucket" {
-  name                     = "tt-githubdemo-bucket-001"
-  project                  = "yashu-project-359213"
-  location                 = "US"
-  force_destroy            = true
-  public_access_prevention = "enforced"
+resource "google_storage_bucket" "static" {
+ name          = "my-first-jenkinexpbucket"
+ location      = "US"
+ storage_class = "STANDARD"
+
+ uniform_bucket_level_access = true
 }
