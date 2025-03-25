1173cdc16601a52c57df60c95434081b93ceb043
iamyasodapuli <yasodapulicsk7@gmail.com>
2025-03-25 12:03:12 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 resource "google_storage_bucket" "static" {
  name          = "my-first-jenkinexpbucket"
- project_number = "yashu-project-359213"
+ project = "yashu-project-359213"
  location      = "US"
  storage_class = "STANDARD"
 
